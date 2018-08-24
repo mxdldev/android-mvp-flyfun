ca9876b1a9c95e842aa9ede272ef375c34e3f802
gxl <geduo_83@163.com>
2018-08-24 14:22:50 +0800

    add ExpandableTextView

--- app/src/main/java/com/yesway/android/MainActivity.java
+++ app/src/main/java/com/yesway/android/MainActivity.java
@@ -6,6 +6,7 @@ import com.yesway.android.net.ApiManager;
 import com.yesway.android.net.dto.response.LoginResponse;
 import com.yesway.android.net.http.Response;
 import com.yesway.android.net.model.IUserModel;
+import com.yesway.android.view.ExpandableTextView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -20,15 +21,8 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent("android.intent.action.second_activity");
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-            }
-        });
-        
+    ExpandableTextView expandableTextView = findViewById(R.id.expand_text_view);
+    expandableTextView.setText("我们为全球首个人工智能区块链数字资产交易平台（全球前十）提供全面技术支持服务\n" + "公司以区块链技术开发应用为核心业务方向，获得一线资本千万美元的投资，全力打造全球区块链行业中，安全、透明的区块链资产技术服务平台是我们的企业使命");
     }
 
     @Override
